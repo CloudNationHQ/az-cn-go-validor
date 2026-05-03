3f47d6c1b05b54207d53ccd30347e2d39717d2d9
dkooll <dennis.kool@cloudnation.nl>
2026-05-03 15:44:20 +0200

    feat: refactor package structure and consolidate tests while preserving behavior

--- runner.go
+++
@@ -1,9 +0,0 @@
-package validor
-
-import (
-	"testing"
-)
-
-func RunTests(t *testing.T, modules []*Module, parallel bool, config *Config) {
-	runModuleTests(t, modules, parallel, config, nil, "registry")
-}
